40696e94a8084a4cc36d0010976dd18091400e3c
dwinurssa <142791230+dwinurssa@users.noreply.github.com>
2023-10-23 16:41:21 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,17 @@
+#Membuat matriks 2x2
+matriksA = [ [1,0], [0,1]]
+matrix = [[5, 0], [2, 6],] 
+print(matriksA)
+print(matrix)
+
+#Membuat matriks 3x3
+matriksB = [ [1,0,1], [0,1,0], [1,0,1] ]
+matrix1 = [[5, 0, 8], [2, 6, 7], [1, 3, 4],] 
+print(matriksB)
+print(matrix1)
+
+#Membuat matriks 4x4
+matriksC = [ [1,0,1,0],[0,1,0,1],[1,0,1,0],[0,1,0,1] ]
+matrix2 = [[1, 0, 0, 0],[0, 1, 0, 0],[0, 0, 1, 0],[0, 0, 0, 1],]
+print(matriksC)
+print(matrix2)
